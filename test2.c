14ab9c7660c0e6ac7f19a186ded56181384762c1
thelord82 <mandrelord@gmail.com>
2022-09-29 16:05:03 -0400

    added echo builtin

---
+++ test2.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test2.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malord <malord@student.42quebec.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/29 09:10:54 by malord            #+#    #+#             */
+/*   Updated: 2022/09/29 13:20:26 by malord           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+int main(void)
+{
+	printf("Retour de readline = %s\n", readline("Enter stuff here : "));
+}
